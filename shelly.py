58af4a822e6339136ce7f01c04717810466aaa95
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2023-10-16 00:26:39 -0400

    GOT PIPES WORKING

--- shelly.py
+++ shelly.py
@@ -121,6 +121,7 @@ class Controller(Host):
         
         try:
             with open(f"./modules/{module}.sh","r") as f:
+                print(f.read())
                 self.send(target['ip'], "module", f.read().encode())
         except FileNotFoundError:
             print("Module does not exist")
