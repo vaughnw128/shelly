5c4a11422efe3f2c73121d2cdad807f825ae5df4
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2023-10-17 17:12:26 -0400

    GOT PIPES WORKING

--- src/shelly.py
+++ src/shelly.py
@@ -250,7 +250,7 @@ class Controller(Host):
             return self.db.all()
         
         # Gets single target
-        target = self.db.search(Query().number == target_num)[0]
+        target = self.db.search(Query().number == target_num)
         if target['status'] == "DISCONNECTED":
             print("This target is not connected")
             return None
